dca472cf260ba3aede940489a764125703ca3e79
srsebbi <csperichon@gmail.com>
2023-04-15 22:05:27 -0300

    Agregar archivos de proyecto.

---
+++ CRUDDotNet/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using CRUDDotNet.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDDotNet.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class PersonController : Controller
+{
+    private readonly DataContext _dataContext;
+
+    public PersonController(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+
+    [HttpGet]
+    public IActionResult GetPersons()
+    {
+        var persons = _dataContext.Persons.ToList();
+        return Ok(persons);
+    }
+    [HttpPost]
+    public IActionResult AddPerson(datos )
+    {
+        agrego guid
+    }
+
+}
+
---
+++ CRUDDotNet/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUDDotNet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDDotNet.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<Person> Persons { get; set; }
+}
+
